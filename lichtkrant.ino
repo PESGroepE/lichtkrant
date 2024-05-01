f5f7daf113d33fba482015b56c85195f6804b483
Alisa <alisavpatlan@gmail.com>
2024-05-01 12:14:53 +0200

    first

---
+++ lichtkrant.ino
@@ -0,0 +1,30 @@
+#include <MD_Parola.h>
+#include <MD_MAX72xx.h>
+#include <SPI.h>
+
+#define HARDWARE_TYPE MD_MAX72XX::FC16_HW
+//#define HARDWARE_TYPE MD_MAX72XX::GENERIC_HW
+
+//Pins defineren
+#define MAX_DEVICES 4 //4 "units" aan schermen van de LED "Lichtkrant"
+#define DATA_PIN D7   //DATA
+#define CS_PIN D8   //CS 
+#define CLK_PIN D5 //CLCK
+
+//initieren van de display 
+MD_Parola lichtkrant = MD_Parola(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
+
+void setup() {
+  lichtkrant.begin(); //
+  lichtkrant.setIntensity(10);
+  lichtkrant.displayClear();
+}
+
+void loop() {
+  lichtkrant.setTextAlignment(PA_LEFT); //display alignment aan de linkerkant
+  char message[] = "User";
+  lichtkrant.print(message); //print "Username"
+  delay(2000);
+  lichtkrant.displayClear();
+
+}
